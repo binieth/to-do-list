9c2cd88d034fa569e6e6cfaa7734731a6a233a25
binieth <abigiyabiniyam@gmail.com>
2023-06-01 11:31:42 -0400

    few random changes here and there

--- src/ToDoListApp.java
+++ src/ToDoListApp.java
@@ -6,17 +6,24 @@ public class ToDoListApp {
     private static JTextField textField;
 
     public static void main(String[] args) {
-        // defining the initial frame of the application.
+        /* Defining the initial frame of the application.
+        * In Java, JFrame is a class from the javax.swing package
+        * that represents a top-level container for creating and
+        * managing a windowed GUI application.*/
 
         JFrame frame = new JFrame("To-Do list App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 600);
+        frame.setSize(350, 400);
         frame.setLayout(new BorderLayout());
-        frame.setVisible(true);
+        frame.setVisible(true); //displays the frame
+        frame.setLocationRelativeTo(null); // centers the frame on the screen
+        frame.setResizable(true); //optional
 
         //adding a JPanel to hold the to-do list items:
         JPanel panel = new JPanel();
+        //panel.setBackground(Color.lightGray); optional
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
 
 
         //Create a JScrollPane to make the list scrollable:
@@ -26,10 +33,11 @@ public class ToDoListApp {
         //Add the scrollPane to the frame:
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        //Add a text field and a button for adding new to-do items. We'll create a separate method called addItem() to handle this functionality:
+        //Add a text field and a button for adding new to-do items. Will need to create a separate method called addItem() to handle this functionality:
         JTextField textField = new JTextField();
-        JButton addButton = new JButton("Add");
         textField.addActionListener(e -> addItem(panel, textField));
+
+        JButton addButton = new JButton("ADD");
         addButton.addActionListener(e -> addItem(panel, textField));
 
         //Add the text field and button to the frame:
